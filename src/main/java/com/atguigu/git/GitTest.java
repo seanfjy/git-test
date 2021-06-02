293b46df6408a359e0f9420b7475d07b730c3802
seanfjy <seanfjy@qq.com>
2021-06-02 17:18:05 +0800

    third commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author sean
+ * @create 2021-06-02 下午 17:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git2");
+        System.out.println("hello Git3");
+    }
+}
